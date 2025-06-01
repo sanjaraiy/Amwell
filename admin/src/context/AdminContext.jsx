462aebda4e868d0057bef7ef40eccb18b5ce920d
sanjaraiy <deepakrai78788@gmail.com>
2025-06-01 14:50:55 +0530

    done doctorlist page with backend integration

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,17 +1,40 @@
+import axios from 'axios';
 import {createContext, useState} from 'react';
+import { toast } from 'react-toastify';
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
    
     const [adminToken, setAdminToken] = useState(localStorage.getItem('adminToken') ? localStorage.getItem('adminToken') : '');
+    const [doctors, setDoctors] = useState([]);
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const getAllDoctors = async () => {
+      try {
+          const res = await axios.get(backendUrl + '/api/v1/admin/all-doctors', {
+             headers: {adminToken: adminToken}
+          })
+          
+          if(res.data.success){
+              setDoctors(res.data.doctors);
+
+          }else{
+            toast.error(res.data.message);
+          }
+
+      } catch (error) {
+         toast.error(error.message);
+      }
+    }
 
    const value = {
        adminToken,
        setAdminToken,
        backendUrl,
+       doctors,
+       getAllDoctors
    }
 
    return (
