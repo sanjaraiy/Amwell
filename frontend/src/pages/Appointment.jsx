f9495fc3ed7c6f29deddef49e058f5308d5ce7f4
sanjaraiy <deepakrai78788@gmail.com>
2025-06-03 11:21:23 +0530

    added appoitment functionality

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,13 +1,17 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import RelatedDoctors from "../components/RelatedDoctors";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Appointment = () => {
   const { docId } = useParams();
-  const { doctors, currencySymbol } = useContext(AppContext);
+  const { doctors, currencySymbol, backendUrl, token, getDoctorsData } = useContext(AppContext);
   const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  const navigate = useNavigate();
 
   const [docInfo, setDocInfo] = useState(null);
   const [docSlots, setDocSlots] = useState([]);
@@ -45,6 +49,23 @@ const Appointment = () => {
       let timeSlots = [];
       while (currentDate < endTime) {
         let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+         
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth()+1;
+        let year = currentDate.getFullYear();
+
+        const slotDate = day + "_" + month + "_" + year;
+        const slotTime = formattedTime;
+
+        const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true;
+        
+        if(isSlotAvailable){
+           timeSlots.push({
+            datetime: new Date(currentDate),
+            time: formattedTime
+           })
+        }
+        
 
         timeSlots.push({
           datetime: new Date(currentDate),
@@ -59,6 +80,35 @@ const Appointment = () => {
 
     setDocSlots(allSlots);
   };
+
+
+  const bookAppointment = async () => {
+    if(!token){
+      toast.warn('Login to book appointment')
+      return navigate('/login');
+    }
+
+    try {
+      const date = docSlots[slotIndex][0].datetime
+
+      let day = date.getDate();
+      let month = date.getMonth()+1;
+      let year = date.getFullYear();
+
+      const slotDate = day + "_" + month + "_" + year;
+
+      const {data} = await axios.post(backendUrl + '/api/user/bool-appointment', {docId, slotDate, slotTime}, {headers: {token}});
+      if(data.success){
+        toast.success(data.message);
+        getDoctorsData();
+        navigate('/my-appointments')
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+       toast.error(error.message);
+    }
+  }
 
   useEffect(() => {
     fetchDocInfo();
@@ -126,7 +176,7 @@ const Appointment = () => {
                 ))
             }
         </div>
-        <button className="bg-[#5f6FFF] text-white text-sm font-light px-14 py-3 rounded-full my-6">Book an appointment</button>
+        <button onClick={bookAppointment} className="bg-[#5f6FFF] text-white text-sm font-light px-14 py-3 rounded-full my-6">Book an appointment</button>
         </div>
       </div>
 
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,26 +1,53 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 const MyAppointments = () => {
    
-    const {doctors} = useContext(AppContext);
+    const {doctors, backendUrl, token} = useContext(AppContext);
+
+    const [appointments, setAppointments] = useState([]);
+    const months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    const slotDateFormat = (slotDate) => {
+         const dateArray = slotDate.split('_');
+         return dateArray[0]+ " " + months[Number(dateArray[1])] + " " + dateArray[2]; 
+    }
+    const getUserAppointment = async () => {
+        try {
+            const {data} = await axios.get(backendUrl+'/api/user/appointments', {headers: {token}});
+
+            if(data.success){
+                setAppointments(data.appointments.resvers())
+            }
+        } catch (error) {
+            toast.error(error.message); 
+        }
+    }
+
+    useEffect(()=> {
+        if(token){
+            getUserAppointment();
+        }
+    }, [token])
 
     return (
         <div>
             <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">My appointments</p>
             <div>
                 {
-                    doctors.slice(0,3).map((item, idx) => (
+                    appointments.map((item, idx) => (
                         <div className="grid grid-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b" key={idx}>
                             <div>
-                                <img className="w-32 bg-indigo-50" src={item.image} alt="" />
+                                <img className="w-32 bg-indigo-50" src={item.docData.image} alt="" />
                             </div>
                             <div className="flex-1 text-sm text-zinc-600">
-                                <p className="text-neutral-800 font-semibold">{item.name}</p>
-                                <p>{item.speciality}</p>
+                                <p className="text-neutral-800 font-semibold">{item.docData.name}</p>
+                                <p>{item.docData.speciality}</p>
                                 <p className="text-zinc-700 font-medium mt-1">Address:</p>
-                                <p className="text-xs">{item.address.line1}</p>
-                                <p className="text-xs">{item.address.line2}</p>
-                                <p className="text-xs mt-1"><span className="text-sm text-neutral-700 font-medium">Date & Time:</span> 25, July, 2024 | 8:30 PM</p>
+                                <p className="text-xs">{item.docData.address.line1}</p>
+                                <p className="text-xs">{item.docData.address.line2}</p>
+                                <p className="text-xs mt-1"><span className="text-sm text-neutral-700 font-medium">Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
                             </div>
                             <div></div>
                             <div className="flex flex-col gap-2 justify-end">
