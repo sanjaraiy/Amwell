1b467852a326cdba090235f410b7fa32e2e60cdb
sanjaraiy <deepakrai78788@gmail.com>
2025-07-04 08:47:35 +0530

    added doctordashboard controller and route

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { doctorsList , loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel} from '../controllers/doctorController.js';
+import { doctorsList , loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard} from '../controllers/doctorController.js';
 import authDoctor from '../middlewares/authDoctor.js';
 
 const router = express.Router();
@@ -7,8 +7,10 @@ const router = express.Router();
 router.get('/doctor-list', doctorsList);
 router.post('/login', loginDoctor);
 router.get('/doctor-appointments', authDoctor, appointmentsDoctor);
-router.post('/complete-appointment', authDoctor, appointmentComplete),
+router.post('/complete-appointment', authDoctor, appointmentComplete);
 router.post('/cancel-appointment', authDoctor, appointmentCancel);
+router.get('/dashboard', authDoctor, doctorDashboard);
+
 
 
 
